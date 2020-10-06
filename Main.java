0fc9ad2b818e0fdc980a6e4f7a4322ed186102db
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-05 23:01:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ *This program prints out the factors of a positive integer 
+ * @author Zach ramirez 
  */
 public class Main {
 
@@ -9,7 +10,37 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in); 
+    
+    //declare the integer variable
+    int integer;
+    //question loop
+    do{ 
+     //ask user for positive integer 
+     System.out.println("Please enter a positive integer to determine its factors:"); 
+      integer = input.nextInt(); 
+     //determine whether the user inputed a positive or negative integer 
+      if(integer >= 0){ 
+      } 
+    }
+    //outside the loop means user has entered positive integer
+    while(integer < 0); 
+    System.out.println("The factors of " + integer + " are: ");
+    
+    //create a variable to keep track of the number 
+    int count = 1;
+    int remainder =  integer % 2;
+    
+    //using a loop to count the factors 
+    while(count <= integer){
+      System.out.println(count); 
+      if(remainder >= 0) 
+      integer = integer + remainder;
+      System.out.println(integer);
+        
+    } 
+
     
   }
 }
